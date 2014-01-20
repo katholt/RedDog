e8e82e97a3ceebf92824974b5e3256ee23745c16
David <d.edwards2@student.unimelb.edu.au>
2014-01-20 11:09:44 +1100

    Fix for chromosome names

--- chrom_info.py
+++ chrom_info.py
@@ -7,7 +7,7 @@ def chromInfoFasta(refFile):
     # extract the chromosome name and length from the fasta reference
     chroms = []
     for record in SeqIO.parse(refFile, "fasta"):
-        name = record.id
+        name = record.name
         if name.find('.') != -1:
             temp_name = name.split('.')
             name = temp_name[0]
@@ -18,7 +18,7 @@ def chromInfoGenbank(refFile):
     # extract the chromosome name and length from the genbank reference
     chroms = []
     for record in SeqIO.parse(refFile, "genbank"):
-        name = record.id
+        name = record.name
         if name.find('.') != -1:
             temp_name = name.split('.')
             name = temp_name[0]
